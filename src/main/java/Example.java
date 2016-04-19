9a94826ac15732205afa0976d3e165ec6dbc4d42
Ganesh Pachpind <pachpindg@gmail.com>
2016-04-19 22:46:22 +0530

    #3 : Simple Rest API - hit url localhost:8080/ returns Hello User

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,11 +1,15 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Component
 @EnableAutoConfiguration
 public class Example {
 
+  @RequestMapping("/")
   public String home() {
     return "Hello User";
   }
