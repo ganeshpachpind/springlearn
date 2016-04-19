28bf403e6a525cf997f2a58a5a3eb601481c6adf
Ganesh Pachpind <pachpindg@gmail.com>
2016-04-19 20:54:32 +0530

    #1 : Basic Application using spring boot - this create simple jar not web application learned : - what dependencies need to add in gradle                - Run app using ./gradlew bootrun                - @EnableAutoConfiguration detects does this app required any web component as we don't have any web dependencies this makes simple jar and run as standalone java application

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,10 +1,17 @@
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableAutoConfiguration
 public class Example {
+
+  public String home() {
+    return "Hello User";
+  }
+
   public static void main(String args[]) {
+    SpringApplication.run(Example.class, args);
     System.out.println("Running this as a simple jar file not as a webapp");
   }
 
