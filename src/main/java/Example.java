7d31fa90aefffcc0907803f6d6373de23dd745a6
Ganesh Pachpind <pachpindg@gmail.com>
2016-04-20 21:44:19 +0530

    #3 : Add Spring Security dependencies

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,7 +1,9 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +14,16 @@ public class Example {
   @RequestMapping("/")
   public String home() {
     return "Hello User";
+  }
+
+  @RequestMapping(value = "/welcome/{name}",method = RequestMethod.GET)
+  public String welcome (@PathVariable("name") String name) {
+   return "Welcome "+ name;
+  }
+
+  @RequestMapping(value = "/SecuredData/{name}",method = RequestMethod.GET)
+  public String securedData (@PathVariable("name") String name) {
+    return name + "Congrats Yor can access Secure Data ";
   }
 
   public static void main(String args[]) {
