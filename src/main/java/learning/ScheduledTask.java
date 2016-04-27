9eb722269b980211b433fa971df102246686d7fe
Ganesh Pachpind <pachpindg@gmail.com>
2016-04-27 22:52:07 +0530

    #5 : Configuration for Scheduler to run parallely on multiple threads
    -- @Schedule annotation - schedules job on single thread executor
    -- We can add configuration to use Executor with threadpool size to run multiple jobs same time in paralllel

--- src/main/java/learning/ScheduledTask.java
+++ src/main/java/learning/ScheduledTask.java
@@ -9,18 +9,21 @@ public class ScheduledTask {
   @Scheduled(fixedRate = 100)
   public void executeFixedRate()
   {
-    System.out.println("Executing every 100 millis time count start at beginning of method execution ");
+//    System.out.println("Executing every 100 millis time count start at beginning of method execution " );
+    System.out.println(Thread.currentThread().getName() + " 100 sec diff " +System.currentTimeMillis());
   }
 
-  @Scheduled(fixedDelay = 100)
-  public void executeFixedDelay()
-  {
-    System.out.println("Executing every 100 millis time count start at end of method execution ");
-  }
+//  @Scheduled(fixedDelay = 100)
+//  public void executeFixedDelay()
+//  {
+////    System.out.println("Executing every 100 millis time count start at end of method execution ");
+//    System.out.println(Thread.currentThread().getName() + " 100 sec diff2 " +System.currentTimeMillis());
+//  }
 
   @Scheduled(cron = "*/1 * * * * *")
-  public void executeCron()
-  {
-    System.out.println("Executing every 1 second ");
+  public void executeCron() throws InterruptedException {
+//    System.out.println("Executing every 1 second " + Thread.currentThread().getName() + Thread.currentThread().isDaemon());
+    System.out.println(Thread.currentThread().getName() + " 1 sec diff " +System.currentTimeMillis());
+    Thread.sleep(200l);
   }
 }
